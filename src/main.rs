76cbdc2145bc64f276143178aa7d29f815d86cf7
y3ll0ww <jelle.vangeel@legrand-ext.com>
2025-09-05 08:09:45 +0200

    Configure development environment through docker compose

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use cache::redis_fairing;
 use routes::{USERS, WORKSPACES};
 
 use crate::routes::PROJECTS;
@@ -20,11 +19,20 @@ pub mod schema;
 #[cfg(test)]
 mod tests;
 
+pub const ENV_REDIS_URL: &str = "REDIS_URL";
+pub const ENV_DATABASE_URL: &str = "DATABASE_URL";
+
 #[launch]
 fn rocket() -> _ {
-    rocket::custom(rocket::Config::figment())
+    // Fetch DATABASE_URL from env
+    let db_url = std::env::var(ENV_DATABASE_URL).expect(&format!("{ENV_DATABASE_URL} must be set"));
+
+    // Merge it into Rocket's config at runtime
+    let figment = rocket::Config::figment().merge(("databases.rustle_db.url", db_url));
+
+    rocket::custom(figment)
         .attach(database::Db::fairing())
-        .attach(redis_fairing())
+        .attach(cache::redis_fairing())
         .mount(PROJECTS, routes::projects::routes())
         .mount(USERS, routes::users::routes())
         .mount(WORKSPACES, routes::workspaces::routes())
