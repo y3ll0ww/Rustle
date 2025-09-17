9b1d9ff6d8c51fa61da8c0053fc14f48e72f20d3
y3ll0ww <jelle.vangeel@legrand-ext.com>
2025-09-17 21:28:04 +0200

    Add endpoint reference in protected route

--- frontend/src/context/ProtectedRoute.jsx
+++ frontend/src/context/ProtectedRoute.jsx
@@ -1,12 +1,13 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "./AuthContext";
+import { Endpoint } from "../utils/EndPoints";
 
 const ProtectedRoute = ({ fallback, children }) => {
   const { user, loading } = useAuth();
 
   if (loading) return <p>Loading...</p>;
   if (!user) {
-    return fallback ? fallback : <Navigate to="/login" replace />;
+    return fallback ? fallback : <Navigate to={Endpoint.login} replace />;
   }
 
   return children;
